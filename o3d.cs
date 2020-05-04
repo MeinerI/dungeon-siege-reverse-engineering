944c51ce52c16d5ec7f9378a55c79df9283bdebd
Man Sun <ramfanderu@mail.ru>
2020-05-04 10:41:11 +0300

    Update o3d.cs

--- o3d.cs
+++ o3d.cs
@@ -1,9 +1,9 @@
 //Console.WriteLine("*");
 
-//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+//Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦
   using System; using System.IO; using System.Linq; using System.Text; using System.Collections; 
   using System.Collections.Generic; using System.Text.RegularExpressions; using System.Globalization; 
-//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+//Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦
 
 sealed class Test
 {
@@ -18,10 +18,10 @@ sealed class Test
 
         foreach (var fileIn in filesName)
         {
-						using (BinaryReader br = new BinaryReader(File.Open(fileIn, FileMode.Open)))
-						{
-								using (StreamWriter sw = new StreamWriter(fileIn + ".obj"))
-								{
+	    using (BinaryReader br = new BinaryReader(File.Open(fileIn, FileMode.Open)))
+	    {
+		using (StreamWriter sw = new StreamWriter(fileIn + ".obj"))
+		{
                     O3dModel o3d = new O3dModel();
 
                     vertexCount = br.ReadUInt32(); 
@@ -70,15 +70,64 @@ sealed class Test
 
                       o3d.faces[i].texNumber = br.ReadUInt16();
                     }
-								}
+
+// ======================================================== 
+
+                    sw.WriteLine("ply");
+                    sw.WriteLine("format ascii 1.0");
+                    //sw.WriteLine("comment TextureFile " + model_texture.jpg);
+                    sw.WriteLine("element vertex " + vertexCount) ; // ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð²ÐµÑ€ÑˆÐ¸Ð½
+
+                    sw.WriteLine("property float x") ;
+                    sw.WriteLine("property float y") ;
+                    sw.WriteLine("property float z") ;
+
+                    //sw.WriteLine("property uchar red") ;
+                    //sw.WriteLine("property uchar green") ;
+                    //sw.WriteLine("property uchar blue") ;
+                    //sw.WriteLine("property uchar alpha") ;
+
+                    foreach (var v in o3d.faces)
+                      if (v.index[3] != 65535)
+                        faceCount++;
+
+                    sw.WriteLine("element face " + faceCount); // Ð¾Ð±Ñ‰ÐµÐµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð³Ñ€Ð°Ð½ÐµÐ¹
+
+                    sw.WriteLine("property list uchar int vertex_indices"); 
+                    sw.WriteLine("property list uchar double texcoord");
+                    //sw.WriteLine("property int texnumber");
+
+                    sw.WriteLine("end_header") ;
+
+                    foreach (var v in o3d.vertexes)
+                    sw.WriteLine(v.x + " " + v.y + " " + v.z);
+			
+		    // Ð±ÑƒÐ´Ñƒ Ñ€Ð°Ð·Ð±Ð¸Ð²Ð°Ñ‚ÑŒ ÐºÐ²Ð°Ð´Ñ‹ Ð½Ð° Ñ‚Ñ€ÐµÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸ÐºÐ¸, Ð¿Ð¾Ñ‚Ð¾Ð¼Ñƒ Ñ‡Ñ‚Ð¾...
+
+                    foreach (var v in o3d.faces)
+                    {
+                        sw.WriteLine("3 " + v.index[0] + " " + v.index[1] + " " + v.index[2] + " 6 " +
+                        + v.texCoords[0].u/255f + " " + v.texCoords[0].v/255f + " " 
+                        + v.texCoords[1].u/255f + " " + v.texCoords[1].v/255f + " " 
+                        + v.texCoords[2].u/255f + " " + v.texCoords[2].v/255f + " " /*+ v.texNumber*/);
+
+                    //  0 1 2 3 => 0 1 2 + 0 2 3
+
+                        if (v.index[3] != 65535)
+                        sw.WriteLine("3 " + v.index[0] + " " + v.index[2] + " " + v.index[3] + " 6 " +
+                        + v.texCoords[0].u/255f + " " + v.texCoords[0].v/255f + " " 
+                        + v.texCoords[2].u/255f + " " + v.texCoords[2].v/255f + " " 
+                        + v.texCoords[3].u/255f + " " + v.texCoords[3].v/255f + " " /*+ v.texNumber*/);
+                    }
+		}
             }
         } // foreach (var file in filesName)
     } // public static void Main()
 } // sealed class Test {
 
-//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
-//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
-//ææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææææ
+//Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦
+//Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦
+//Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦Ã¦
 
 public class O3dModel
 {
@@ -107,9 +156,9 @@ public class o3d_color
 
 // ======================================================== 
  
-public class o3d_face // ãðàíü/ïîëèãîí (òðåóãîëüíèê èëè ÷åòûðåõóãîëüíèê)
+public class o3d_face // Ã£Ã°Ã Ã­Ã¼/Ã¯Ã®Ã«Ã¨Ã£Ã®Ã­ (Ã²Ã°Ã¥Ã³Ã£Ã®Ã«Ã¼Ã­Ã¨Ãª Ã¨Ã«Ã¨ Ã·Ã¥Ã²Ã»Ã°Ã¥ÃµÃ³Ã£Ã®Ã«Ã¼Ã­Ã¨Ãª)
 {
-  public o3d_color color = new o3d_color();  //  BGR(A) face color, âèäèìî.
+  public o3d_color color = new o3d_color();  //  BGR(A) face color, Ã¢Ã¨Ã¤Ã¨Ã¬Ã®.
   public o3d_texcoord[] texCoords = new o3d_texcoord[4];
   public ushort[] index = new ushort[4];
   public uint unknown;
